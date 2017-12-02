22947a3b19c83d077a3c7e40675cc6af478dc4ba
Travis CI User <travis@example.org>
2017-12-02 22:24:32 +0000

    Update documentation

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["travis_bot"] = {"doc":"This is a comment.","items":[[5,"always_true","travis_bot","This function always returns true. It's very useful!",null,{"inputs":[],"output":{"name":"bool"}}]],"paths":[]};
+initSearch(searchIndex);
