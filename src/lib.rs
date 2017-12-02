0c44b599e4effb66cf6cbd3c21305d4b3b8dca28
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2017-12-02 22:57:49 +0100

    Update doc location

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #![crate_name = "travis_bot"]
 #![crate_type = "lib"]
 
+#![doc(html_root_url = "https://kyeett.github.io/travis-bot/")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/kyeett/travis-bot/master/demo.png")]
+
+
 //! This is a comment.
 
 /// This function always returns true. It's very useful!
